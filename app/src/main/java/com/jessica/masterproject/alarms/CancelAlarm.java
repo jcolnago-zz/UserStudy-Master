ec95d00c84646642d586427755f85eb7447cb9c5
jcolnago <jessicacolnago@gmail.com>
2016-02-05 09:54:55 -0500

    Revert "Changes to text"
    This reverts commit bfa26214790a7501f6d40f36dfe540be062a00c7.

--- app/src/main/java/com/jessica/masterproject/alarms/CancelAlarm.java
+++ app/src/main/java/com/jessica/masterproject/alarms/CancelAlarm.java
@@ -34,7 +34,7 @@ public class CancelAlarm extends BroadcastReceiver {
 
             int missed = mSharedPref.getInt(context.getString(R.string.missed_interruptions), 0);
             mEditor.putInt(context.getString(R.string.missed_interruptions), ++missed);
-            // set it as done, so it won't try to upload a non-existing file.
+            // set it as done, so it won't try to upload an non-existing file.
             mEditor.putBoolean(context.getString(R.string.upload_done) + filename, true);
             mEditor.commit();
 
